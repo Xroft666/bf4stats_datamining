f0d077c1480313ef52a2da0dff09332b4ee07783
Victor Apihtin <vapyhtin@gmail.com>
2014-04-26 15:42:06 +0200

    gui added with players selection

--- unity_project/Assets/scripts/bf4stats.cs
+++ unity_project/Assets/scripts/bf4stats.cs
@@ -5,22 +5,27 @@ using System.Collections.Generic;
 public class bf4stats : MonoBehaviour 
 {
     public int numberOfPages = 1;
-    private TextMesh textMesh;
+	private List<string> names = new List<string>(); 
+
     private string leaderboardURI = "http://bf4stats.com/leaderboards/pc_player_score?start=";
+	private string playerInfoURI = "http://api.bf4stats.com/api/playerInfo?plat=pc&name=";
 
-    void Awake()
-    {
-        textMesh = GetComponent<TextMesh>();
-    }
+	private string currentPlayerInfo = "";
 
-    IEnumerator Start()
+	private Vector2 namesScrollView;
+	private Vector2 playerInfoScrollView;
+
+	IEnumerator Start()
+	{
+		yield return StartCoroutine(RetriveNames());
+	}
+
+    IEnumerator RetriveNames()
     {
         string nameLineStart = "<a href=\"/pc/";
 		string nameLineEnd = "</a>";
 
-        List<string> names = new List<string>(); 
-
-		print ("Loading...");
+		print ("Retriving names...");
 
         for( int i = 0; i < numberOfPages; i++ )
             using (WWW www = new WWW(leaderboardURI + i) )
@@ -39,10 +44,45 @@ public class bf4stats : MonoBehaviour
 					names.Add( www.text.Substring(startIndex + 2, endIndex - startIndex - 2) );
             	}
         }
-	
-		foreach( string name in names )
-			print (name);
 
 		print ("" + names.Count + " names crawled in: " + Time.time + " seconds.");
     }
+
+	IEnumerator RetrivePlayerInfo(string name)
+	{
+		string outputFormat = "&output=js";
+
+		print ("Retriving player info on " + name + "...");
+
+		using (WWW www = new WWW(playerInfoURI + name + outputFormat) )
+		{
+			yield return www;
+			currentPlayerInfo = www.text;
+		}
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginHorizontal();
+
+		GUILayout.BeginVertical();
+
+		namesScrollView = GUILayout.BeginScrollView(namesScrollView);
+		foreach(string name in names )
+		{
+			if( GUILayout.Button(name) )
+			{
+				StartCoroutine(RetrivePlayerInfo(name));
+			}
+		}
+		GUILayout.EndScrollView();
+
+		GUILayout.EndVertical();
+
+		playerInfoScrollView = GUILayout.BeginScrollView(playerInfoScrollView);
+		GUILayout.Label(currentPlayerInfo);
+		GUILayout.EndScrollView();
+
+		GUILayout.EndHorizontal();
+	}
 }
