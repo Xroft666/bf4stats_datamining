7c487bd8438c31cc33683c0129f8fb0ce4593ca7
Victor Apihtin <vapyhtin@gmail.com>
2014-05-20 04:32:40 +0200

    reading esom files

--- unity_project/Assets/scripts/CreateLearningData.cs
+++ unity_project/Assets/scripts/CreateLearningData.cs
@@ -3,27 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
 
 public class CreateLearningData : MonoBehaviour {
 
 	//public TextAsset dataFile;
 	public string outputFileName;
 
-
 	public static CreateLearningData instance;
 	void Awake(){
 		instance = this;
 		Application.targetFrameRate = 0;
-	}
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	public IEnumerator LearnDataFromTuples(List<Tuple> tupleList){
@@ -63,5 +54,58 @@ public class CreateLearningData : MonoBehaviour {
 		SoundReference.instance.PlaySound(SoundReference.instance.Done);
 
 	}
-	
+
+	static public void ReadClusterFile(string tuplesFileName, string esomFileName)
+	{
+		string filePath = Application.dataPath + "/Data/ESOMResult/" + esomFileName + ".cls";
+		string tuplesPath = Application.dataPath + "/Data/Tuples/" + tuplesFileName + ".txt";
+
+		string[] fileLines = File.ReadAllLines (filePath);
+		string jsonTuples = File.ReadAllText (tuplesPath);
+
+		List<Tuple> tuples = JsonConvert.DeserializeObject<List<Tuple>>(jsonTuples);
+
+		// going to the first player id
+		for (int i = 0; i < fileLines.Length; i++) 
+		{
+			if (fileLines [i] [0] == '%')
+				continue;
+
+			string[] lineElements = fileLines [i].Split('\t');
+			int playerId = int.Parse(lineElements[0]);
+			int playerCluster = int.Parse(lineElements[1]);
+
+			for( int j = 0; j < tuples.Count; j++ )
+				if( tuples[j].playerID == playerId )
+					tuples[j].playerCluster = playerCluster;
+		}
+
+		// sorting by claster
+		tuples = tuples.OrderBy (element => element.playerCluster);
+
+		// searching for the average weapon in the cluster
+		Dictionary<int, string> weaponsClusterMap = new Dictionary<int, string> ();
+
+		int clusterIndex = tuples[0].playerCluster;
+		for( int i = 0; i < tuples.Count; i++ )
+		{
+			Dictionary<string, int> weaponsCounter = new Dictionary<string, int>();
+
+			bool hasChanged = clusterIndex != tuples[i].playerCluster;
+			clusterIndex = tuples[i].playerCluster;
+
+			if( !weaponsCounter.ContainsKey(tuples[i].favWeapon) )
+				weaponsCounter[tuples[i].favWeapon] = 0;
+
+			weaponsCounter[tuples[i].favWeapon]++;
+
+			if( hasChanged )
+			{
+				weaponsClusterMap[clusterIndex] = weaponsCounter.OrderByDescending( x => x.Value ).First().Key;
+				weaponsCounter.Clear();
+			}
+		}
+
+		// At this point weaponsClusterMap will have the most used guns of each clusters
+	}
 }
